1817a046adbdc8b99362d30a96ba5d2e5555b6b1
jrgiacone <giaconejoshua@gmail.com>
2022-06-05 18:30:48 -0700

    Added cors functionallity

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const { response } = require('express')
 const express = require('express')
 const app = express ()
+const cors = require('cors')
 const PORT = 3000
 
+app.use(cors())
 
 const coffeeMakers = {
   'pour over':{
