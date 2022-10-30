d87a2f328805415bb7e7fcebd4b438bf60000dee
jrgiacone <giaconejoshua@gmail.com>
2022-10-29 20:04:45 -0700

    some css styling for the buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import AuthProvider from "../contexts/AuthContext";
 import Signup from "./Authentication/Signup";
 import Login from "./Authentication/Login";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import PrivateRoute from "./Authentication/PrivateRoute";
 import ForgotPassword from "./Authentication/ForgotPassword";
 import UpdateProfile from "./Authentication/UpdateProfile";
@@ -9,29 +9,29 @@ import Header from "./Header";
 import Selection from "./Selection";
 import Coffee from "./Coffee";
 import Profile from "./Profile";
-import Nav from "./Nav" 
+import Nav from "./Nav"
 
 function App() {
   return (
-            <Router>
-              <AuthProvider>
-                <Routes>
-                  {/* <Route exact path ='/' element={<PrivateRoute><Profile /></PrivateRoute>} /> */}
-                  <Route path='/' exact element={
-                      <PrivateRoute> 
-                        <Nav />
-                        <Header /> 
-                        <Selection />
-                      </PrivateRoute>} />
-                  <Route path ='/update-profile' element={<PrivateRoute><UpdateProfile /></PrivateRoute>} />
-                  <Route path="/signup" element={<Signup/>} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/profile" element={<Profile />} />
-                  <Route path="/forgot-password" element={<ForgotPassword />} />
-                  <Route path="/coffee" element={<Coffee />} />
-                </Routes>
-              </AuthProvider>
-            </Router>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          {/* <Route exact path ='/' element={<PrivateRoute><Profile /></PrivateRoute>} /> */}
+          <Route path='/' exact element={
+            <PrivateRoute>
+              <Nav />
+              <Header />
+              <Selection />
+            </PrivateRoute>} />
+          <Route path='/update-profile' element={<PrivateRoute><UpdateProfile /></PrivateRoute>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/coffee" element={<Coffee />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
